3359d72608a0c5494eb725c6174b4af63ad5f51d
khaled87 <khaled.nawasreh@gmail.com>
2025-10-01 11:06:08 +0200

    add Order function

---
+++ src/PaymentSystem/shoppingCart/Item.java
@@ -0,0 +1,26 @@
+package PaymentSystem.shoppingCart;
+
+import java.util.*;
+
+// Item class for eShop catalog
+class Item {
+    private int id;
+    private String name;
+    private double price;
+
+    public Item(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    @Override
+    public String toString() {
+        return id + ": " + name + " ($" + price + ")";
+    }
+}
---
+++ src/PaymentSystem/shoppingCart/OrderManager.java
@@ -0,0 +1,37 @@
+package PaymentSystem.shoppingCart;
+
+import java.util.*;
+
+class OrderManager {
+    private Map<Integer, Order> orders = new HashMap<>();
+
+    // Create
+    public void addOrder(Order order) {
+        orders.put(order.getOrderId(), order);
+    }
+
+    // Read
+    public Order getOrder(int orderId) {
+        return orders.get(orderId);
+    }
+
+    // Update
+    public void updateOrder(int orderId, List<Item> updatedItems) {
+        Order order = orders.get(orderId);
+        if (order != null) {
+            order.setItems(updatedItems);
+        }
+    }
+
+    // Delete
+    public void deleteOrder(int orderId) {
+        orders.remove(orderId);
+    }
+
+    public void printAllOrders() {
+        for (Order order : orders.values()) {
+            System.out.println(order);
+            System.out.println("-----");
+        }
+    }
+}
