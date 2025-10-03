2087ebf617b685eb997a2717fd1d400ce8f7645f
Lenanor <nordstromlena@hotmail.com>
2025-10-03 14:59:42 +0200

    move discount on receipt

--- src/PaymentSystem/Payment/InvoiceFormatter.java
+++ src/PaymentSystem/Payment/InvoiceFormatter.java
@@ -12,9 +12,9 @@ public class InvoiceFormatter implements ReceiptFormatter{
         sb.append("Invoice number: ").append(receipt.getInvoiceNumber()).append("\n");
         sb.append("Terms of payment: ").append(receipt.getTerms()).append("\n\n");
         sb.append(receipt.shoppingItemsToString()).append("\n");
+        sb.append(String.format("%-7s %15.2fKr\n", "Rabatt: ", receipt.getDiscountSum()));
         sb.append((String.format("%-7s %16.2fKr\n", "TOTAL: ", receipt.getTotal())));
         sb.append("==========================\n");
-        sb.append("Rabatt: ").append(receipt.getDiscountSum()).append("\n");
         sb.append(String.format("Payment method: %s\n", receipt.getPaymentMethod()));
         sb.append("Contact: ").append(receipt.getPhoneNumber()).append("\n");
 
